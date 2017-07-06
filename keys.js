3e6787c5a93a137f09904b7bef6f6ed6596c5661
Kristen Bowen <kbowen200247@gmail.com>
2017-07-06 17:34:48 -0400

    take out things

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'hJH971S5cxgjOZGZC1XWaGaxn',
-  consumer_secret: 'DkGnRnF4RT3TuY33kIjyzAWApFG1MwQCZMvzcft3CMOhxmerJE',
-  access_token_key: '3254787156-vap2GFMzqtjZ6PcwczxQrbtBEoVah0NFFa8EtFm',
-  access_token_secret: '7Q3fktg7nGtHKNEVvp3fuUPxZKyIrqtnLKbImEtVvL14h',
-}
